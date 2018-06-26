ed22311391a879b6544ee81a1dfddd7ea97ac80e
meepingsnesroms <guicrith@gmail.com>
2018-06-26 11:50:55 -0700

    New test, minor cleanups and bugfixes

---
+++ decompilations/HwrIRQ5Handler.c
@@ -0,0 +1,51 @@
+/*
+ROM:10083AC0 ; =============== S U B R O U T I N E =======================================
+ROM:10083AC0
+ROM:10083AC0 ; Attributes: bp-based frame
+ROM:10083AC0
+ROM:10083AC0 sub_10083AC0:                           ; DATA XREF: sub_10084132+28↓o
+ROM:10083AC0
+ROM:10083AC0 var_4           = -4
+ROM:10083AC0
+ROM:10083AC0                 link    a6,#-4          ; Link and Allocate
+ROM:10083AC4                 move.l  d3,-(sp)        ; Move Data from Source to Destination
+ROM:10083AC6                 tst.b   (byte_253).w    ; Test an Operand
+ROM:10083ACA                 bne.s   loc_10083AF8    ; Branch if Not Equal
+ROM:10083ACC                 pea     var_4(a6)       ; Push Effective Address
+ROM:10083AD0                 trap    #$F             ; Trap sysTrapPenGetRawPen
+ROM:10083AD0                 dc.w    $A270
+ROM:10083AD4                 move.w  d0,d3           ; Move Data from Source to Destination
+ROM:10083AD6                 addq.w  #4,sp           ; Add Quick
+ROM:10083AD8                 bne.s   loc_10083AE4    ; Branch if Not Equal
+ROM:10083ADA                 pea     var_4(a6)       ; Push Effective Address
+ROM:10083ADE                 trap    #$F             ; Trap sysTrapEvtEnqueuePenPoint
+ROM:10083ADE                 dc.w    $A126
+ROM:10083AE2                 addq.w  #4,sp           ; Add Quick
+ROM:10083AE4
+ROM:10083AE4 loc_10083AE4:                           ; CODE XREF: sub_10083AC0+18↑j
+ROM:10083AE4                 tst.b   (byte_253).w    ; Test an Operand
+ROM:10083AE8                 beq.s   loc_10083AF8    ; Branch if Equal
+ROM:10083AEA                 pea     (1).w           ; Push Effective Address
+ROM:10083AEE                 move.l  (dword_14E).w,-(sp) ; Move Data from Source to Destination
+ROM:10083AF2                 trap    #$F             ; Trap sysTrapSysTimerWrite
+ROM:10083AF2                 dc.w    $A09A
+ROM:10083AF6                 addq.w  #8,sp           ; Add Quick
+ROM:10083AF8
+ROM:10083AF8 loc_10083AF8:                           ; CODE XREF: sub_10083AC0+A↑j
+ROM:10083AF8                                         ; sub_10083AC0+28↑j
+ROM:10083AF8                 move.l  (sp)+,d3        ; Move Data from Source to Destination
+ROM:10083AFA                 unlk    a6              ; Unlink
+ROM:10083AFC                 rts                     ; Return from Subroutine
+ROM:10083AFC ; End of function sub_10083AC0
+ROM:10083AFC
+ROM:10083AFC ; ---------------------------------------------------------------------------
+ROM:10083AFE                 dc.b $8E
+ROM:10083AFF aHwrirq5handler:dc.b 'HwrIRQ5Handler',0
+ROM:10083B0E                 align $10
+*/
+
+void HwrIRQ5Handler(){
+   if(*((uint16_t*)0x00000253) != 0){
+      //not done
+   }
+}
---
+++ decompilations/PrvSetBB7846ChanRef.c
@@ -0,0 +1,41 @@
+/*
+ROM:10005D9C sub_10005D9C:                           ; CODE XREF: sub_10005F0C+45E↓p
+ROM:10005D9C
+ROM:10005D9C arg_0           =  8
+ROM:10005D9C
+ROM:10005D9C                 link    a6,#0           ; Link and Allocate
+ROM:10005DA0                 movea.l #$FFFFF000,a0   ; Move Address
+ROM:10005DA6                 move.w  #$4247,($FFFFF802).w ; Move Data from Source to Destination
+ROM:10005DAC                 move.w  arg_0(a6),d0    ; Move Data from Source to Destination
+ROM:10005DB0                 andi.w  #$FFFE,d0       ; AND Immediate
+ROM:10005DB4                 move.w  d0,($FFFFF800).w ; Move Data from Source to Destination
+ROM:10005DB8                 move.w  #$4347,($FFFFF802).w ; Move Data from Source to Destination
+ROM:10005DBE
+ROM:10005DBE loc_10005DBE:                           ; CODE XREF: sub_10005D9C+2A↓j
+ROM:10005DBE                 move.w  $802(a0),d0     ; Move Data from Source to Destination
+ROM:10005DC2                 andi.w  #$100,d0        ; AND Immediate
+ROM:10005DC6                 bne.s   loc_10005DBE    ; Branch if Not Equal
+ROM:10005DC8                 move.w  ($FFFFF800).w,d0 ; Move Data from Source to Destination
+ROM:10005DCC                 move.w  #$E000,($FFFFF802).w ; Move Data from Source to Destination
+ROM:10005DD2                 unlk    a6              ; Unlink
+ROM:10005DD4                 rts                     ; Return from Subroutine
+ROM:10005DD4 ; End of function sub_10005D9C
+ROM:10005DD4
+ROM:10005DD4 ; ---------------------------------------------------------------------------
+ROM:10005DD6                 dc.w $9350
+ROM:10005DD8 aRvsetbb7846cha:dc.b 'rvSetBB7846ChanRef',0
+ROM:10005DEB                 align 4
+*/
+
+uint16_t PrvSetBB7846ChanRef(uint16_t channel){
+   uint16_t returnValue;
+
+   *((uint16_t*)0xFFFFF802) = 0x4247;//enables SPI, required for the write to SPIDATA2 to go through
+   *((uint16_t*)0xFFFFF800) = channel & 0xFFFE;
+   *((uint16_t*)0xFFFFF802) = 0x4347;//transfer 8 bits
+   while(*((uint16_t*)0xFFFFF802) & 0x100);//busy wait for transfer to finish
+   /*an interrupt occurs here from the transfer finishing if SPI2 is not masked in IMR*/
+   returnValue = *((uint16_t*)0xFFFFF800);
+   *((uint16_t*)0xFFFFF802) = 0xE000;//disable and set clock rate to lowest possible(don't know why since its off anyway)
+   return returnValue;
+}
